192048741fbefa7517ea27675d39aa29ebfa3d61
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-03-04 15:50:40 -0800

    removed redundant comments

--- fb-callback.php
+++ fb-callback.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/src/Facebook/autoload.php';
-//include"common.php";
+
 $fb = new Facebook\Facebook([
   'app_id' => '1817429491842129',
   'app_secret' => '7176bc285dd5a7b4b7097c25751ce725',
@@ -28,7 +28,7 @@ $fb = new Facebook\Facebook([
   if (isset($accessToken)) {
     // Logged in!
     $_SESSION['facebook_access_token'] = (string) $accessToken;
-    //alert($target);
+
     // Now you can redirect to another page and use the
     // access token from $_SESSION['facebook_access_token']
     $self_path = "http://".$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
